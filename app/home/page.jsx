21a9b23c58c433eb16a0e7727838ead62ff89e98
nanven <icedeerwanna@outlook.com>
2023-10-29 19:35:29 +0800

    fix ssr

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -1,4 +1,4 @@
-import { cache } from 'react'
+import { Suspense, cache } from 'react'
 
 import { getSortedPostsData, getAllClassification } from '../../utils/posts';
 import HomeLayout from './client-layout';
@@ -14,7 +14,9 @@ export default async function Home() {
   const allClassification = await getStaticAllClassification();
 
   return (
-    <HomeLayout allPostsData={allPostsData} allClassification={allClassification} />
+    <Suspense fallback={<HomeLayoutFallback allPostsData={allPostsData} />}>
+      <HomeLayout allPostsData={allPostsData} allClassification={allClassification} />
+    </Suspense>
   )
 
 }
@@ -30,3 +32,31 @@ export const getStaticSortedPostsData = cache(async () => {
   return allPostsData;
 
 })
+
+function HomeLayoutFallback({ allPostsData }) {
+  return (
+    <div className={style.homeBase}>
+      <h2 className={style.homeTitle}>全部文章</h2>
+      <div className={style.homeContainer}>
+        {allPostsData.map(({ id, classRoute, classification, title, description, image, date }) => (
+          <div key={id} className={style.acticleContainer}>
+            <Link className={style.acticle} href={`/${classRoute}/${id}`}>
+              <div className={style.acticleHeader}>
+                <div className={style.titleContainer}>
+                  <h2 className={style.title}>{title}</h2>
+                  <span className={style.classification}>{classification}</span>
+                </div>
+                <div className={style.descriptionContainer}>
+                  <div className={style.date}>{date}</div>
+                  <div className={style.description}>{description}</div>
+                </div>
+              </div>
+              <LoadingImg rectangle className={`${style.acticleImage} useDarkFilter`} src={image} />
+
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
