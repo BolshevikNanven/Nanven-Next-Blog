3eb8ac521c2ae45c3427dc8cd3c3b7b853117d14
nanven <icedeerwanna@outlook.com>
2024-12-29 16:58:41 +0800

    fix: component function name

--- app/[classification]/[id]/nav-bar.jsx
+++ app/[classification]/[id]/nav-bar.jsx
@@ -3,7 +3,7 @@ import MarkdownNavbar from 'markdown-navbar'
 
 import style from '@/styles/article.module.css'
 
-export default function ({content}) {
+export default function NavBar ({content}) {
     return (
         <MarkdownNavbar className={style.navBar} ordered={false} headingTopOffset={40} source={content} />
     )
